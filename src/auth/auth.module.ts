5d52b46723843141fca7fa784acee64a2c75dbee
moonclash <emladenoff@gmail.com>
2022-11-24 17:01:07 +0100

    hold this code for now

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { AuthController } from './auth.controller';
 @Module({
   imports: [PassportModule, 
     JwtModule.register({
-      secret: process.env.JWT_SECRET,
+      secret: 'secret',
       signOptions: {
         expiresIn: 3600,
       }
